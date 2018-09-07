96760407f4ef268d3a10acd691be89ea9c5d8d4e
Farzad <hfarsath@gmail.com>
2018-09-07 22:41:51 +0530

    music app with favorite

--- RailwayTracking/music/models.py
+++ RailwayTracking/music/models.py
@@ -5,10 +5,13 @@ class Album(models.Model):
     Lyricist=models.CharField(max_length=250)
     Album_title=models.CharField(max_length=500)
     Composer=models.CharField(max_length=250)
-
+    Album_logo=models.CharField(max_length=1000)
     def __str__(self):
-        return self.Album_title + '-' + self.Composer
+        return self.Album_title 
 
 class Song(models.Model):
     album=models.ForeignKey(Album,on_delete=models.CASCADE)
     song_title=models.CharField(max_length=500)
+    is_favorite=models.BooleanField(default=False)
+    def __str__(self):
+        return self.song_title
--- RailwayTracking/music/urls.py
+++ RailwayTracking/music/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+app_name='music'
 urlpatterns = [
     
     path('',views.index,name='index'),
+    path('<album_id>/',views.detail,name='detail'),
+     path('<album_id>/favorite/',views.favorite,name='favorite'),
 
 
 ]
